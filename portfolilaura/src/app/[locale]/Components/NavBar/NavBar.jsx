60229a6186606a3b7771273a2c0fe38ab37df2e9
Marcos Ramirez <gellollo@outlook.com>
2024-02-22 13:49:52 -0300

    NavBar responsive

--- portfolilaura/src/app/[locale]/Components/NavBar/NavBar.jsx
+++ portfolilaura/src/app/[locale]/Components/NavBar/NavBar.jsx
@@ -1,8 +1,9 @@
 'use client'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import	LauraLogo from '../../../../Data/LogoLaura.png'
 import { FaLinkedin } from "react-icons/fa";
+import { SlArrowLeftCircle } from "react-icons/sl";
 import { FaCalendarAlt } from "react-icons/fa";
 import Swal from 'sweetalert2'
 import { IoIosMail } from "react-icons/io";
@@ -11,6 +12,8 @@ import style from './NavBar.module.css'
 
 export default function NavBar({SectionAbout, SectionProjects}) {
   const [stateBar, setStateBar] = useState(false)
+  const [navBarState, setNavBarState] = useState(true)
+  const [isMobile, setIsMobile] = useState(false);
   const showSwal = () => {
 
     Swal.fire({
@@ -38,7 +41,16 @@ const CalendlyLink = 'https://calendly.com/lauramirezcarri'
     }
   }
 
-
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768); 
+    };
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => {
+      window.removeEventListener('resize', checkMobile);
+    };
+  }, []);
   
   const handleClick = () =>{
     if( stateBar === false){
@@ -49,21 +61,29 @@ const CalendlyLink = 'https://calendly.com/lauramirezcarri'
     }
   }
   return (
-    <div className="fixed w-[100vw] h-24 bg-offSalmon-200 flex flex-row shadow-sm justify-between shadow-salmon-200 select-none p-3 z-[100]">
-        <div className="flex flex-col place-self-start relative left-8">
-        <Image className="rounded-full h-40 w-40 p-2 bg-offSalmon-300 ring-8 ring-offSalmon-200" src={LauraLogo} width={300} height={300} alt="PhLogo"/>
+    <div className={`fixed w-32 h-[100vh] lg:w-[100vw] lg:h-24 bg-offSalmon-200 flex flex-col lg:flex lg:flex-row shadow-sm lg:justify-between shadow-salmon-200 select-none p-3 ease-in-out duration-300 z-[100]
+     ${
+      navBarState ? 'translate-x-0' : 'translate-x-[-50vw]'
+    }`}>
+      {
+        isMobile ? <div className="relative justify-self-end flex flex-col">
+<button className="font-spartan text-3xl text-offSalmon-800 p-5"><SlArrowLeftCircle/></button>
+        </div> : null
+      }
+        <div className="flex flex-col pb-5 place-self-start relative left-8">
+        <Image className="rounded-full  h-40 w-40 p-2 bg-offSalmon-300 ring-8 ring-offSalmon-200" src={LauraLogo} width={300} height={300} alt="PhLogo"/>
         </div>     
-<div className="flex flex-row relative gap-16 justify-self-end right-4">
-  <button onClick={()=> scrollToSection1()} className="text-2xl text-center font-normal font-spartan transition-color ease-in duration-150 underline decoration-offSalmon-200  hover:decoration-salmon-800">Sobre mi</button>
-  <button onClick={()=> scrollToSection2()} className="text-2xl text-center font-normal font-spartan transition-color ease-in duration-150 underline decoration-offSalmon-200  hover:decoration-salmon-800 ">Mis Copys</button>
-  <Link className="relative top-5" href='/Laura_Ramírez_Carrivali_CV.pdf' download target="_blank"><button className="text-2xl text-center font-normal font-spartan transition-color ease-in duration-150 underline decoration-offSalmon-200  hover:decoration-salmon-800">Descargar CV</button></Link>
+<div className="flex flex-col lg:flex lg:flex-row relative gap-10 lg:gap-16 justify-self-end lg:right-4">
+  <button onClick={()=> scrollToSection1()} className="text-lg lg:text-2xl text-center font-normal font-spartan transition-color ease-in duration-150 underline decoration-offSalmon-200  hover:decoration-salmon-800">Sobre mi</button>
+  <button onClick={()=> scrollToSection2()} className="text-lg lg:text-2xl text-center font-normal font-spartan transition-color ease-in duration-150 underline decoration-offSalmon-200  hover:decoration-salmon-800 ">Mis Copys</button>
+  <Link className="relative left-1 lg:top-5" href='/Laura_Ramírez_Carrivali_CV.pdf' download target="_blank"><button className="text-lg lg:text-2xl text-center font-normal font-spartan transition-color ease-in duration-150 underline decoration-offSalmon-200  hover:decoration-salmon-800">Descargar CV</button></Link>
   <div className="flex flex-col self-center h-8 w-40">
-  <button onClick={() => handleClick()} className="text-2xl text-center font-normal font-spartan transition-color ease-in duration-150 underline decoration-offSalmon-200  hover:decoration-salmon-800">¡Contactame!</button>
+  <button onClick={() => handleClick()} className="text-lg lg:text-2xl text-center font-normal font-spartan transition-color ease-in duration-150 underline decoration-offSalmon-200  hover:decoration-salmon-800">¡Contactame!</button>
   {stateBar ?<div className={style.animate}>
-    <div className="flex flex-col absolute top-2 justify-self-start bg-offSalmon-100 rounded p-4 transition-all divide-y divide-offSalmon-50 gap-2">
-    <a href={LinkedinLink} target="_blank"> <button  className="text-2xl text-center font-normal gap-3 flex flex-row font-spartan transition-color ease-in duration-150 hover:text-salmon-800"><FaLinkedin/> Linkedin</button></a>
- <a href={MailLink} target="_blank"><button onClick={() => showSwal()} className="text-2xl text-center font-normal gap-3 flex flex-row font-spartan transition-color ease-in duration-150 hover:text-salmon-800"><IoIosMail/> Correo</button></a>
- <a href={CalendlyLink} target="_blank"><button className="text-2xl text-center font-normal gap-3 flex flex-row font-spartan transition-color ease-in duration-150 hover:text-salmon-800"><FaCalendarAlt/>Meeting</button></a>
+    <div className="flex flex-col absolute left-2 top-2 justify-self-start bg-offSalmon-100 rounded p-4 transition-all divide-y divide-offSalmon-50 gap-2">
+    <a href={LinkedinLink} target="_blank"> <button  className="text-md lg:text-2xl text-center font-normal gap-3 flex flex-row font-spartan transition-color ease-in duration-150 hover:text-salmon-800"><FaLinkedin/> Linkedin</button></a>
+ <a href={MailLink} target="_blank"><button onClick={() => showSwal()} className="text-md lg:text-2xltext-center font-normal gap-3 flex flex-row font-spartan transition-color ease-in duration-150 hover:text-salmon-800"><IoIosMail/> Correo</button></a>
+ <a href={CalendlyLink} target="_blank"><button className="text-md lg:text-2xl text-center font-normal gap-3 flex flex-row font-spartan transition-color ease-in duration-150 hover:text-salmon-800"><FaCalendarAlt/>Meeting</button></a>
   </div>
   </div> 
    :null
